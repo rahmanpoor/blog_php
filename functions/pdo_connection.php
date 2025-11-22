68c76a43bcced050c1be986b2ceba4442631b2a7
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-11-22 07:53:58 +0330

    project config bugfix

--- functions/pdo_connection.php
+++ functions/pdo_connection.php
@@ -3,7 +3,7 @@
 global $pdo;
 try {
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ);
-    $pdo = new PDO('mysql:host=quirky-brattain-p8gmpujjz-db;dbname=hungry_antonelli','root','oZaqWQkN8sbLxWMbUNKOWjOx',$options);
+    $pdo = new PDO('mysql:host=quirky-brattain-p8gmpujjz-db;port=3306;dbname=hungry_antonelli','root','oZaqWQkN8sbLxWMbUNKOWjOx',$options);
     return $pdo;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
